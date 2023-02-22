7b74465f2a28975fe537aa01ade6a7567b796e9d
Your Name <you@example.com>
2023-02-22 01:16:02 -0800

    Task1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - cause an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	/**
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("infinite loop avoided! \\0/\n");
+
+	return (0);
+}
