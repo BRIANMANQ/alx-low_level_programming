0772fdae9ac55e49a5d3778ef3079b45b334e818
Your Name <you@example.com>
2023-03-03 03:09:21 -0800

    Task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,10 @@
 #include "main.h"
-
 /**
- * _strcat - concatennates two strings
- * @dest: copy to
- * @src: copy from
- * Return: pointer to dest
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
  */
 char *_strcat(char *dest, char *src)
 {
@@ -12,12 +12,18 @@ char *_strcat(char *dest, char *src)
 	int j;
 
 	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
 	}
-	dest[i] != '\0';
+
+	dest[i] = '\0';
 	return (dest);
 }
